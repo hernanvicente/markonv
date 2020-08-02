7625c9f56f144718e126d51ff809fb2186561c33
Hernan Maguina <hernan.maguina@gmail.com>
2020-08-02 20:42:45 +0200

    Convert paragraphs

--- markonv.rb
+++ markonv.rb
@@ -1,16 +1,30 @@
 class Markonv
   def initialize(markdown)
     @markdown = markdown
+    @result = nil
   end
 
   def parse
     prepare
-    @markdown
+    convert_paragraphs
+    remove_empty_lines
+    @result
+
   end
 
   private
 
   def prepare
-    @markdown.strip!
+    @result = @markdown.strip!
+  end
+
+  def convert_paragraphs
+    @result = @result.gsub(/[^\r\n]+((\r|\n|\r\n)[^\r\n]+)*/) do |match|
+      "<p>#{match.strip}</p>"
+    end
+  end
+
+  def remove_empty_lines
+    @result.gsub!(/^[\n]/, '')
   end
 end
--- markonv_test.rb
+++ markonv_test.rb
@@ -15,10 +15,19 @@ class MarkonvTest < Minitest::Test
 
       A final paragraph
     MARKDOWN
+
+    @html = <<~HTML.strip
+      <p>And **strong** and **bold** are strong tags.</p>
+      <p>And this is the second paragraph</p>
+      <p>And this is the third paragraph and a list:</p>
+      <p>* One
+      * Two</p>
+      <p>A final paragraph</p>
+    HTML
   end
 
   def test_markdown
     markonv = Markonv.new(@markdown)
-    assert_equal @markdown, markonv.parse
+    assert_equal @html, markonv.parse
   end
 end
